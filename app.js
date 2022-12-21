9a8680036e44870e19200dece2d641d2cc780c35
mun <munstarrrrr@gmail.com>
2022-12-21 20:48:45 +0900

    1221 강문호 commit 3

---
+++ app.js
@@ -0,0 +1,10 @@
+function moveBear() {
+    bear = document.getElementsByClassName('bear')[0];
+    bear.classList.add('show_bear');
+}
+
+window.onload = function() {
+    // $('section').on('click',moveBear);
+    section = document.getElementsByTagName('section')[0];
+    section.addEventListener('mouseover',moveBear);
+}
